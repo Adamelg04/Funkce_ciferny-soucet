45895d6559fd1996c0934b6d441cd85047a304ca
adame <adame@DESKTOP-VOPV20U>
2022-02-27 20:27:01 +0100

    Přidejte soubory projektu.

---
+++ Funkce_ciferny soucet.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <math.h>
+void main()
+{
+	int x;
+	int soucet = 0;
+	int i;
+	printf("Zadejte cislo pro ciferny soucet: ");
+	scanf_s("%d", &x);
+	for (i=0; x > 0;i++)
+	{
+		i = x % 10;
+		x = x / 10;
+		soucet = soucet + i;
+	}
+	printf("%d", soucet);
+	return 0;
+
+}
